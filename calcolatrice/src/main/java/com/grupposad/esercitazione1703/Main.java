51c15a55114cd555250316705f90fbfb01cef4a4
00mc22 <michelecoscarelli@icloud.com>
2025-03-17 15:33:20 +0100

    Main

--- calcolatrice/src/main/java/com/grupposad/esercitazione1703/Main.java
+++ calcolatrice/src/main/java/com/grupposad/esercitazione1703/Main.java
@@ -1,7 +1,9 @@
 package com.grupposad.esercitazione1703;
+import com.grupposad.esercitazione1703.calculator.CalculatorUI;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        CalculatorUI calculatorUI = new CalculatorUI();
+        calculatorUI.commandLineTool();
     }
 }
